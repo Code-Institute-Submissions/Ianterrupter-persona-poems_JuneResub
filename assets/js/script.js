63382ccc82d6d7fa180e4e0ab2599ef4aace635a
Ianterrupter <iandowney94@gmail.com>
2022-05-02 13:49:48 +0000

    Many javascript changes - saved user answers, created poem element, added restart button, added hidden/visible logic for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const questionsList = [
     {
         question: "Which of the below movies best describes you?",
         options: ["Pulp Fiction", "The Notebook", "The Hangover", "Die Hard"],
-        answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4' ]
+        answers: ["You enjoy the more quirky things in life, ", "A lifelong romantic,", 'Answer 3', 'Answer 4' ]
     },
     {
         question : "Which of the below books best describes you?",
@@ -32,7 +32,6 @@ const questionsList = [
 // Question Counter
 
 let questionCounter = 0;
-const questionNumber = options.length;
 
 // Array of Answers
 
@@ -41,35 +40,110 @@ let answers = [];
 // Questions
 
 let question = document.getElementById("main-question");
-
 let options = document.getElementsByClassName("question-answer");
+let optionsList = document.getElementById("answers");
+let buttons = document.getElementById("nav-buttons");
 
+let optionCount = options.length;
 function questionDisplay() {
     
     question.innerHTML = questionsList[questionCounter].question;
 
-    options[0].innerHTML = questionsList[questionCounter].options[0];
-    options[1].innerHTML = questionsList[questionCounter].options[1];
-    options[2].innerHTML = questionsList[questionCounter].options[2];
-    options[3].innerHTML = questionsList[questionCounter].options[3];
+    for (let i = 0; i < optionCount; i++) {
+        let option = options[i];
+        option.innerHTML = questionsList[questionCounter].options[i];
+        // This is for saving the answer so we know what answer was clicked
+        option.dataset.indexNumber = i;
+    }
+
+    // Event for saving answers
+    let buttons = document.getElementsByClassName("question-answer");
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', saveAnswer);
+    }
+
+    // This function is called after saveAnswer so that the array is updated before nextQuestion
+    for (let i = 0; i < optionCount; i++) 
+    {
+        options[i].addEventListener('click', nextQuestion);
+    }
 }
 
 // Poem
 
 function displayPoem() {
+
+    question.innerHTML = "Thank you for your time..."
+
+    // Makes poem...
+
+    let heading = "<h3>... here's a little poem based on your answers!</h3>";
+
+    for (let i = 0; i < answers.length; i++) {
+        let index = answers[i];
+        let question = questionsList[i];
+        let answer = question['answers'][index];
+        heading += '<p>' + answer + '</p>';
+    }
+
+    let poem = document.createElement("div");
+
+    poem.setAttribute("id","poem");
+
+    poem.innerHTML = heading;
+
+    question.appendChild(poem);
+
+    // Removes answers...
+
+    optionsList.style.visibility = "hidden";
+
+    // Hides buttons...
+
+    navigationLeft.style.visibility = "hidden";
+    navigationRight.style.visibility = "hidden";
+
+    let restartButton = document.createElement("button");
+
+    restartButton.setAttribute("id","restart-button");
+
+    restartButton.innerHTML = "<i class='fa-solid fa-arrow-rotate-left'></i>";
+
+    question.appendChild(restartButton);
+
+    restartButton.addEventListener('click', restartQuestions);
+   
+}
+
+function restartQuestions() {
     
+    questionCounter = 0;
+    answers = [];
+
+    optionsList.style.visibility = "visible";
+
+    navigationLeft.style.visibility = "visible";
+    navigationRight.style.visibility = "visible";
+
+    let restartButton = document.getElementById("restart-button");
+    restartButton.remove();
+
+    let headingRemove = document.getElementsByTagName("h3")[0];
+    headingRemove.remove();
+
+    questionDisplay();
 }
 
 // Navigation
 
 function nextQuestion() {
-    if (questionCounter > 5) {
+    questionCounter++;
+    
+    if (questionCounter > optionCount) {
         displayPoem();
-    } else {
-        questionCounter++;
-        questionDisplay();
+        return;
     }
-
+    questionDisplay();
 }
 
 let navigationRight = document.getElementById("navigation-right");
@@ -87,20 +161,12 @@ let navigationLeft = document.getElementById("navigation-left");
 
 navigationLeft.addEventListener('click', previousQuestion);
 
-
-questionDisplay();
-
 // Stored Answers
 
-let answersProvided = Array(5).fill("No Answer Provided");
-
-function storeAnswers(option) {
-    answersProvided[questionCounter] = option.innerHTML;
-    nextQuestion();
+function saveAnswer() {
+    answers[questionCounter] = this.dataset.indexNumber;
 }
 
-for (let i = 0; i < options.length; i++) {
-
-    options[i].addEventListener('click', storeAnswers());
-}
-
+window.addEventListener('load', (event) => {
+    questionDisplay();
+});
